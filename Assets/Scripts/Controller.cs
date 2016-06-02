3653f87eee083d425678c4303f35b357c459f251
new-00-0ne <tigers1512@gmail.com>
2016-06-02 17:47:31 -0400

    changed from single joystick to double joystick....still no rotation though

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,38 +8,19 @@ using UnityStandardAssets.CrossPlatformInput;
 #endif
 
 
-public class Controller : MonoBehaviour {
-
+public class Controller : MonoBehaviour
+{
 	public float moveSpeed = 6;
 
     Camera viewCamera;
-
-/*#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_WEBGL*/
 	
     Rigidbody rigidBody;
 	Vector3 velocity;
     
-/*#endif
-
-#if UNITY_ANDROID || UNITY_IOS
-
-    Rigidbody myBody;
-	Vector3 moveVec;
-
-#endif*/
     // Use this for initialization
-    void Start () {
-//#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_WEBGL
-		
+    void Start ()
+    {	
         rigidBody = GetComponent<Rigidbody> ();
-
-/*#endif
-
-#if UNITY_ANDROID || UNITY_IOS
-      
-        myBody = GetComponent<Rigidbody>();
-        
-#endif*/
         viewCamera = Camera.main;
 	}
 	
@@ -59,11 +40,8 @@ public class Controller : MonoBehaviour {
 
 #if UNITY_ANDROID || UNITY_IOS
 
-		/*moveVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"), 0) * moveSpeed;
-		Debug.Log(moveVec);
-		myBody.AddForce(moveVec);*/
-
-        Vector3 mPos = viewCamera.ScreenToWorldPoint(new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"), viewCamera.transform.position.y));
+        // Vector3 mPos = viewCamera.ScreenToWorldPoint(new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"), viewCamera.transform.position.y));
+        Vector3 mPos = viewCamera.ScreenToWorldPoint(new Vector3(CrossPlatformInputManager.GetAxis("Mouse_X"), CrossPlatformInputManager.GetAxis("Mouse_Y"), viewCamera.transform.position.y));
         transform.LookAt(mPos + Vector3.up * transform.position.y);
         velocity = new Vector3(CrossPlatformInputManager.GetAxisRaw("Horizontal"), 0, CrossPlatformInputManager.GetAxisRaw("Vertical")).normalized * moveSpeed;
 
@@ -73,17 +51,6 @@ public class Controller : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-//#if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_WEBGL
-
 		rigidBody.MovePosition (rigidBody.position + velocity * Time.fixedDeltaTime);
-
-/*#endif
-
-#if UNTIY_ANDROID || UNITY_IOS
-
-		myBody.MovePosition (myBody.ImagePosition + moveVec * Time.fixedDeltaTime);
-
-#endif*/
-
     }
 }
