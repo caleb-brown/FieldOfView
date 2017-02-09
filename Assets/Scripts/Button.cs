bae8393cc32c91924389b75f30ed8f677a1f62fb
Caleb Brown <caleb.matthew.brown@gmail.com>
2017-02-09 08:39:41 -0500

    Trying to redo the input system so there is no joystick and the user can touch anywhere on the screen to control the player

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestScene
+{
+    public class Button : MonoBehaviour
+    {
+
+        public Color defaultColor;
+        public Color selectColor;
+        public float moveSpeed = 6.0f;
+
+        Material mat;
+        Camera viewCamera;
+        Rigidbody rb;
+        Vector3 velocity;
+
+        void Awake()
+        {
+            mat = GetComponent<Renderer>().material;
+            rb = GetComponent<Rigidbody>();
+            viewCamera = Camera.main;
+        }
+
+        void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        public void OnTouchUp()
+        {
+            Debug.Log("OnTouchUp");
+            mat.color = defaultColor;
+            velocity = Vector3.zero;
+        }
+
+        public void OnTouch(Vector3 pos)
+        {
+            mat.color = selectColor;
+
+            Vector3 mPos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
+            transform.LookAt(mPos + Vector3.up * transform.position.y * Time.deltaTime);
+            velocity = new Vector3(pos.x, pos.y, 0.0f).normalized * moveSpeed;
+            Debug.DrawRay(transform.position, velocity);
+        }
+
+        void OnTouchExit()
+        {
+            mat.color = defaultColor;
+        }
+    }
+}
