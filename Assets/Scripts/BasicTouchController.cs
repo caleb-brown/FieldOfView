c86dfe685149c995ae8f1414323651c298450acc
Caleb Brown <caleb.matthew.brown@gmail.com>
2017-02-13 11:37:06 -0500

    I could continue befor the controller was working the correct way...It was driving me nuts haha But the controller still isn't perfect but I think I'm on the right path

--- Assets/Scripts/BasicTouchController.cs
+++ Assets/Scripts/BasicTouchController.cs
@@ -38,11 +38,11 @@ public class BasicTouchController : MonoBehaviour
     public void OnTouch(Vector3 pos)
     {
         mat.color = selectColor;
-        Vector3 bVector = new Vector3(pos.x - transform.position.x, 0.0f, pos.z - transform.position.z);
-        // Debug.DrawRay(transform.position, bVector, Color.red);
-        transform.rotation = Quaternion.LookRotation(bVector + Vector3.right * transform.position.z * Time.deltaTime);
-        velocity = new Vector3(bVector.x, 0.0f, bVector.z).normalized * moveSpeed;
-        if (bVector.magnitude <= deadZone)
+        Vector3 mouseVector = new Vector3(pos.x - transform.position.x, 0.0f, pos.z - transform.position.z);
+        Debug.DrawRay(transform.position, transform.position + mouseVector, Color.red);
+        transform.rotation = Quaternion.LookRotation((transform.position + mouseVector) + Vector3.right * transform.position.z * Time.deltaTime);
+        velocity = new Vector3((mouseVector.x), 0.0f, (mouseVector.z)).normalized * moveSpeed;
+        if (mouseVector.magnitude <= deadZone)
         {
             velocity = Vector3.zero;
         }
--- Assets/Scripts/BasicTouchInput.cs
+++ Assets/Scripts/BasicTouchInput.cs
@@ -6,17 +6,32 @@ public class BasicTouchInput : MonoBehaviour
 {
     public BasicTouchController player;
 
+    Vector3 touchPos;
+
     void Update()
     {
 #if UNITY_EDITOR
         if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
         {
-            Vector3 pos = GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(GetComponent<Camera>().transform.position.z)));
+            // I don't need the vector from the camera to the touch position
+            // I need the vector from the touch position to where it has been moved to            
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                touchPos = GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(GetComponent<Camera>().transform.position.y)));
+            }
 
             if (Input.GetMouseButton(0))
             {
-                player.OnTouch(pos);
-                // Debug.DrawRay(transform.position, pos);
+                Vector3 mousePosInWorldSpace = GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(GetComponent<Camera>().transform.position.y)));
+                Vector3 movePos = mousePosInWorldSpace - touchPos;
+
+                player.OnTouch(movePos);
+                Debug.DrawLine(touchPos, mousePosInWorldSpace);
+                // Debug.DrawRay(transform.position, movePos, Color.magenta);
+                // Debug.DrawRay(touchPos, mousePosInWorldSpace, Color.blue);
+                // Debug.DrawRay(transform.position, touchPos);
+                // Debug.Log((touchPos + movePos).magnitude);
             }
             if (Input.GetMouseButtonUp(0))
             {
